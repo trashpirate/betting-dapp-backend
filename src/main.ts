78c7a4a91f0974ee36749652122d9166c46fde42
trashpirate <nadinaoates@gmail.com>
2023-08-28 10:29:57 -0500

    adjust vecel config

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   
+  
   // app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('PetLFG Betting DAPP')
