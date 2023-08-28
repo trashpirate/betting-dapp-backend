48ed805fc758248b7bad5c7866eef8ea768f6535
trashpirate <nadinaoates@gmail.com>
2023-08-28 14:08:57 -0500

    adjust heroku config

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  await app.listen(process.env.PORT || 3001);
   // await app.listen(3001);
 }
 bootstrap();
